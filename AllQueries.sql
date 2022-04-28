283e0bc3a9d69a10996adb9f168bc194f188903e
xeskpau <45764112+xeskpau@users.noreply.github.com>
2022-04-29 00:00:02 +0200

    Update AllQueries.sql
    Update "Task 1".

--- AllQueries.sql
+++ AllQueries.sql
@@ -5,11 +5,11 @@
 # Task 1.
 WITH
   # Get product key for the product of interest.
-  # Note: There are three product keys for this product: 212, 213, 214.
-  # Only the last key is set as "current".
+  # Note: There are three product keys for this product:
+  #       212, 213, 214. Only the last key is set as "current".
   Products AS (
     SELECT DISTINCT ProductKey
-    FROM rullansabater.github.DimProduct
+    FROM rullansabater.remote.DimProduct
     WHERE ProductName = 'Sport-100 Helmet, Red'
   ),
   ResellerSales AS (
@@ -18,7 +18,7 @@ WITH
       SalesAmount,
       DATE(OrderDate) AS OrderDate,
       EXTRACT(MONTH FROM OrderDate) AS `Month`,
-    FROM rullansabater.github.FactResellerSales
+    FROM rullansabater.remote.FactResellerSales
     WHERE EXTRACT(YEAR FROM OrderDate) = 2012
   ),
   SubsetSales AS (
@@ -31,8 +31,9 @@ WITH
       RANK() OVER (
         PARTITION BY ResellerSales.`Month`
         ORDER BY SalesAmount DESC) AS rank_
+    # Use `INNER JOIN` to keep only data for the product
+   # of interest, and no other records from either table.
     FROM ResellerSales
-    # Do `INNER JOIN` to keep only data for the product of interest.
     INNER JOIN Products
       ON ResellerSales.ProductKey = Products.ProductKey
   )
